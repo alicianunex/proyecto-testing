da01a3dc4ddc3628b12294ac2849a9833b2d091e
alicianunezisaac <alicianunez10492@gmail.com>
2021-11-04 00:08:42 +0100

    manufacturer 77%  category 88%

---
+++ src/test/java/com/example/proyectotesting/ManufacturerRestControllerTest.java
@@ -0,0 +1,86 @@
+package com.example.proyectotesting;
+
+
+import com.example.proyectotesting.controller.rest.ManufacturerRestController;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import org.springframework.http.*;
+import com.example.proyectotesting.entities.Manufacturer;
+import com.example.proyectotesting.service.ManufacturerService;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class ManufacturerRestControllerTest {
+
+    private static final String URL = "/api/manufacturers";
+
+    private ManufacturerService manufacturerService;
+
+    private TestRestTemplate restController;
+
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:" + port);
+        restController = new TestRestTemplate(restTemplateBuilder);
+
+        this.manufacturerService = mock(ManufacturerService.class);
+
+        new ManufacturerRestController(manufacturerService);
+
+
+    }
+
+    @Nested
+    @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    public class Find {
+
+        @Test
+        void findAll() {
+
+            List<Manufacturer> arrayList = new ArrayList<>();
+            arrayList.add(new Manufacturer());
+            arrayList.add(new Manufacturer());
+            when(manufacturerService.findAll()).thenReturn(arrayList);
+
+            ResponseEntity<Manufacturer[]> response = restController.getForEntity(URL, Manufacturer[].class);
+            assertAll(
+                    () ->assertEquals(200, response.getStatusCodeValue()),
+                    () ->assertEquals(HttpStatus.OK, response.getStatusCode()),
+                    () ->assertEquals(HttpStatus.valueOf(200), response.getStatusCode()),
+                    () ->assertTrue(response.hasBody()),
+                    () ->assertNotNull(response.getBody()));
+
+            List<Manufacturer> Manufacturers = List.of(response.getBody());
+            assertNotNull(Manufacturers);
+
+            assertTrue(Manufacturers.size() >= 2);
+        }
+
+
+
+
+    }
+
+
+}

cb7b15fbf3c1ddeca176b999730b16d8259eaf17
KarimJesusGalvez <kjagalvez@gmail.com>
2021-11-17 22:27:59 +0100

    Cucumber start

---
+++ src/test/java/com/example/proyectotesting/service/Steps/ManufacturerServiceSteps.java
@@ -0,0 +1,16 @@
+package com.example.proyectotesting.service.Steps;
+
+public class ManufacturerServiceSteps {
+
+    public void generar_producto(){
+
+    }
+
+    public void hacer_click() {
+
+
+    }
+    public void guardar_producto(){
+
+    }
+}

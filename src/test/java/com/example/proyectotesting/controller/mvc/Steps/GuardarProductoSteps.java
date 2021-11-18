e64f93dc8f0ab7680d3c4fc543f7e20b52552b46
KarimJesusGalvez <kjagalvez@gmail.com>
2021-11-18 15:01:39 +0100

    Cucumber structure

---
+++ src/test/java/com/example/proyectotesting/controller/mvc/Steps/GuardarProductoSteps.java
@@ -0,0 +1,18 @@
+package com.example.proyectotesting.controller.mvc.Steps;
+
+
+import com.example.proyectotesting.controller.mvc.ProductListTest;
+import io.cucumber.junit.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/Cucumber.Features/GuardarProducto.feature")
+public class GuardarProductoSteps {
+
+
+    //@When("Click on products")
+    void example(){
+
+        //@Guardar
+        //ProductListTest.save();
+
+    }
+}
--- src/test/resources/Cucumber.Features/GuardarProducto.feature
+++ src/test/resources/Cucumber.Features/GuardarProducto.feature
@@ -1,3 +1,12 @@
 Feature
 
+  Guardar el producto en __
+
+  @Guardar
   Scenario:
+
+    @Given
+    Given The index URL
+    @When
+    When Click on products
+    Then Save product
